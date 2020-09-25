2b9bd539d015849f78f23b82baaf36268b023cbe
Julien <Nisouc@outlook.com>
2020-09-25 17:47:32 +0200

    allo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-document.getElementsByClassName(".openNav").addEventListener('click', showNav());
 
 
-function showNav() {
 
-    document.querySelector(".openNav").id = "shown";
-    console.log("sexe");
-}
+    let icones = document.getElementsByClassName(".openNav");
+
+
+    // for (let i = 0 ; i < icones.length; i++) {
+    //     icones.addEventListener("click", showNav());
+    // }
+
+    // document.querySelector("i").addEventListener("click", showNav());
+
+    // function showNav() {
+
+    //     document.querySelector(".burger").id = "shown";
+    //     console.log("cc");
+
+        
+    // }
 
