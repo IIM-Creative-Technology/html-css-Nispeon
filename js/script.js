97936eae1468eb08c88e5ed78546b231e92691a2
Julien <Nisouc@outlook.com>
2020-09-25 16:48:36 +0200

    add all

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName(".openNav").addEventListener('click', showNav());
+
+
+function showNav() {
+
+    document.querySelector(".openNav").id = "shown";
+    console.log("sexe");
+}
+
