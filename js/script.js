5092c389f05d00eed677df221dc0c4dc3ef6fc99
Julien <Nisouc@outlook.com>
2020-09-25 18:12:31 +0200

    burger

--- js/script.js
+++ js/script.js
@@ -8,13 +8,18 @@
     //     icones.addEventListener("click", showNav());
     // }
 
-    // document.querySelector("i").addEventListener("click", showNav());
+    document.querySelector("i").addEventListener("click", showNav);
 
-    // function showNav() {
+    function showNav() {
 
-    //     document.querySelector(".burger").id = "shown";
-    //     console.log("cc");
+        document.querySelector(".burger").id = "shown";
+        console.log("cc");
 
         
-    // }
+    }
 
+    window.addEventListener('click', function(e){
+        if (!document.querySelector('header').contains(e.target)){
+            document.querySelector(".burger").id = "hidden";
+        }
+    });
