cd5aa6ac34c7183f277994823a7fad81c5ce1ad9
Julien <Nisouc@outlook.com>
2020-09-26 15:20:21 +0200

    fuck up ?

---
+++ headerNav.php
@@ -0,0 +1,18 @@
+<header class="burger" id="hidden">
+
+            <nav>
+                
+                <div><a href="">Profil</a></div>
+                <div><i class="fas fa-user openNav" id="1"></i></div>
+
+                <div><a href="">Groupes</a></div>
+                <div><i class="fas fa-users openNav" id="2"></i></div>
+
+                <div><a href="">Calendrier</a></div>
+                <div><i class="far fa-calendar-alt openNav" id="3"></i></div>
+               
+            </nav>
+
+            <section class="void"></section>
+
+        </header>
---
+++ poll.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta author="Julien Cousin-Alliot">
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Voter !</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/79d4761c9b.js" crossorigin="anonymous"></script>
+
+    </head>
+
+    <body class="pollBody">
+        
+    <?php include 'headerNav.php' ?>
+
+
+
+    </body>
+</html>
