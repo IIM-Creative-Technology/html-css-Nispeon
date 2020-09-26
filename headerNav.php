fb5ba066c84281f0ed3049051f5cf24af8d889c5
Julien <Nisouc@outlook.com>
2020-09-26 18:40:07 +0200

    save day 2

--- headerNav.php
+++ headerNav.php
@@ -2,16 +2,26 @@
 
     <nav>
         
-        <div><a href="">Profil</a></div>
-        <div><i class="fas fa-user openNav"></i></div>
+        <div>
+            <div><a href="">Profil</a></div>
+            <div><i class="fas fa-user openNav"></i></div>
+        </div>
 
-        <div><a href="">Groupes</a></div>
-        <div><i class="fas fa-users openNav"></i></div>
+        <div>
+            <div><a href="">Groupes</a></div>
+            <div><i class="fas fa-users openNav"></i></div>
+        </div>
 
-        <div><a href="">Calendrier</a></div>
-        <div><i class="far fa-calendar-alt openNav"></i></div>
+        <div>
+            <div><a href="">Calendrier</a></div>
+            <div><i class="far fa-calendar-alt openNav"></i></div>
+        </div>
 
-        <div><img src="img/icon.png"></div>
+
+        <div>
+            <div><img src="img/brandnamewhite.png"></div>
+            <div><a href="index.php"><img src="img/icon.png"></a></div>
+        </div>
         
     </nav>
 
