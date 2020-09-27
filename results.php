dc405df891dc279c71d8078d8aa014ac6ea04e67
Julien <Nisouc@outlook.com>
2020-09-27 21:32:23 +0200

    answers added

--- results.php
+++ results.php
@@ -25,7 +25,7 @@
 
                     <div><h2>Classement général</h2></div>
 
-                    <div id="user">
+                    <div>
                         <div><p>1.</p></div> 
                         <div><h3>Nisouc</h3></div> 
                         <div><p>100 pts</p></div> 
@@ -37,7 +37,7 @@
                         <div><p>95 pts</p></div> 
                     </div>
 
-                    <div>
+                    <div id="user">
                         <div><p>3.</p></div>  
                         <div><h3>Lollydroop</h3></div>  
                         <div><p>80 pts</p></div> 
@@ -60,6 +60,31 @@
                         <div><h3>Skupy</h3></div>  
                         <div><p>40 pts</p></div> 
                     </div>
+
+                    <div>
+                        <div><p>7.</p></div>  
+                        <div><h3>Mentalo</h3></div>  
+                        <div><p>35 pts</p></div> 
+                    </div>
+
+                    <div>
+                        <div><p>8.</p></div>  
+                        <div><h3>Leolio</h3></div>  
+                        <div><p>25 pts</p></div> 
+                    </div>
+
+                    <div>
+                        <div><p>9.</p></div>  
+                        <div><h3>Zebulon</h3></div>  
+                        <div><p>20 pts</p></div> 
+                    </div>
+
+                    <div>
+                        <div><p>10.</p></div>  
+                        <div><h3>Albert</h3></div>  
+                        <div><p>0 pts</p></div> 
+                    </div>
+
                 </div>
 
                 <div class="scorePers">
@@ -77,6 +102,99 @@
 
 
                     </div>
+
+                </div>
+
+            </section>
+
+            <section class="pollScore">
+
+                <div class="resultsTitle">
+
+                    <h1>Vos réponses</h1>
+
+                </div>
+
+                <div class="answers">
+
+                    <ol>
+                        <li class="correct">
+                            <div>
+                                <p>Énoncé de la question</p>
+                            </div>
+
+                            <div>
+                                <p>Réponse donnée</p>
+                            </div>
+
+                            <div>
+                                <p>+20 pts</p>
+                            </div>
+
+                        </li>
+
+                        <li class="correct">
+                            <div>
+                                <p>Énoncé de la question</p>
+                            </div>
+
+                            <div>
+                                <p>Réponse donnée</p>
+                            </div>
+
+                            <div>
+                                <p>+20 pts</p>
+                            </div>
+
+                        </li>
+
+                        <li class="correct">
+                            <div>
+                                <p>Énoncé de la question</p>
+                            </div>
+
+                            <div>
+                                <p>Réponse donnée</p>
+                            </div>
+
+                            <div>
+                                <p>+20 pts</p>
+                            </div>
+
+                        </li>
+
+                        <li class="wrong">
+                            <div>
+                                <p>Énoncé de la question</p>
+                            </div>
+
+                            <div>
+                                <p>Réponse donnée</p>
+                            </div>
+
+                            <div>
+                                <p>+0 pts</p>
+                            </div>
+
+                        </li>
+
+                        <li class="correct">
+                            <div>
+                                <p>Énoncé de la question</p>
+                            </div>
+
+                            <div>
+                                <p>Réponse donnée</p>
+                            </div>
+
+                            <div>
+                                <p>+20 pts</p>
+                            </div>
+
+                        </li>
+
+                    </ol>
+
 
                 </div>
 
