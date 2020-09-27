04c9e9195bd4a966bb0a8389ba18998b12cc2370
Julien <Nisouc@outlook.com>
2020-09-27 20:09:01 +0200

    colored sb

--- results.php
+++ results.php
@@ -24,40 +24,40 @@
 
                     <div><h2>Classement général</h2></div>
 
-                    <div>
-                        <div class="mini"><p>1.</p></div> 
-                        <div class="mini"><h3>Nisouc</h3></div> 
-                        <div class="mini"><p>100 pts</p></div> 
+                    <div id="user">
+                        <div><p>1.</p></div> 
+                        <div><h3>Nisouc</h3></div> 
+                        <div><p>100 pts</p></div> 
                     </div>
 
                     <div>
-                        <div class="mini"><p>2.</p></div>  
-                        <div class="mini"><h3>A$ura</h3></div>  
-                        <div class="mini"><p>95 pts</p></div> 
+                        <div><p>2.</p></div>  
+                        <div><h3>A$ura</h3></div>  
+                        <div><p>95 pts</p></div> 
                     </div>
 
                     <div>
-                        <div class="mini"><p>3.</p></div>  
-                        <div class="mini"><h3>Lollydroop</h3></div>  
-                        <div class="mini"><p>80 pts</p></div> 
+                        <div><p>3.</p></div>  
+                        <div><h3>Lollydroop</h3></div>  
+                        <div><p>80 pts</p></div> 
                     </div>
 
                     <div>
-                        <div class="mini"><p>4.</p></div>  
-                        <div class="mini"><h3>Zekray</h3></div>  
-                        <div class="mini"><p>75 pts</p></div> 
+                        <div><p>4.</p></div>  
+                        <div><h3>Zekray</h3></div>  
+                        <div><p>75 pts</p></div> 
                     </div>
 
                     <div>
-                        <div class="mini"><p>5.</p></div>  
-                        <div class="mini"><h3>Creepail</h3></div>  
-                        <div class="mini"><p>60 pts</p></div> 
+                        <div><p>5.</p></div>  
+                        <div><h3>Creepail</h3></div>  
+                        <div><p>60 pts</p></div> 
                     </div>
 
                     <div>
-                        <div class="mini"><p>6.</p></div>  
-                        <div class="mini"><h3>Skupy</h3></div>  
-                        <div class="mini"><p>40 pts</p></div> 
+                        <div><p>6.</p></div>  
+                        <div><h3>Skupy</h3></div>  
+                        <div><p>40 pts</p></div> 
                     </div>
                 </div>
 
