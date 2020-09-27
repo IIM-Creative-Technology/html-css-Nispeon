c416df1a87e3035b3673960f4885d4b926124da5
Julien <Nisouc@outlook.com>
2020-09-27 18:44:36 +0200

    before:scoreboard

--- results.php
+++ results.php
@@ -10,16 +10,37 @@
         <?php include 'headerNav.php' ?>
 
         <main>
+
+            <section class="pollResults">
+
+                <div class="resultsTitle">
+
+                    <h1>Résultats du sondage</h1>
+
+
+                </div>
+
+                <div class="scoreboard">
+
+                </div>
+
+                <div class="scorePers">
+
+                </div>
+
+            </section>
         
         
         
         
+
+
         
         </main>
 
         
 
 
-        
+
     </body>
 </html>
