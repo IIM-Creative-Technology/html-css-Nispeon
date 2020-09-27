03d7e50c7ef5e1a3900aaf6c1bb4085cdd6c54ca
Julien <Nisouc@outlook.com>
2020-09-28 01:01:16 +0200

    save

--- index.php
+++ index.php
@@ -8,79 +8,79 @@
     </head>
     <body class="accueilBody">
 
-    <?php include 'headerNav.php' ?>
+        <?php include 'headerNav.php' ?>
 
-        <main class="indexMain">
+            <main class="indexMain">
 
-            <section class="category">
-
-
-                <div class="tag">
-
-                    <h1>#OverwatchLeague</h1>
-
-                </div>
-
-                <div class="taglist">
-
-                    <a href="poll.php"><div class="pollList"><h2>Quelle équipe va gagner ce soir ?</h2></div></a>
-
-                    <a href="poll.php"><div class="pollList"><h2>Quelle équipe deviendra championne ?</h2></div></a>
-
-                    <a href="poll.php"><div class="pollList"><h2>Quelle joueur sera MVP de la saison ?</h2></div></a>
+                <section class="category">
 
 
-                </div>
+                    <div class="tag">
 
-            </section>
+                        <h1>#OverwatchLeague</h1>
 
-            <section class="category">
+                    </div>
 
+                    <div class="taglist">
 
-                <div class="tag">
+                        <a href="poll.php"><div class="pollList"><h2>Quelle équipe va gagner ce soir ?</h2></div></a>
 
-                    <h1>#KohLanta</h1>
+                        <a href="poll.php"><div class="pollList"><h2>Quelle équipe deviendra championne ?</h2></div></a>
 
-                </div>
-
-                <div class="taglist">
-
-                    <a href="poll.php"><div class="pollList"><h2>Quelle équipe va gagner ce soir ?</h2></div></a>
-
-                    <a href="poll.php"><div class="pollList"><h2>Qui va se faire éliminer ?</h2></div></a>
+                        <a href="poll.php"><div class="pollList"><h2>Quelle joueur sera MVP de la saison ?</h2></div></a>
 
 
-                </div>
+                    </div>
 
-            </section>
+                </section>
 
-            <section class="category">
-
-
-                <div class="tag">
-
-                    <h1>#Formule1</h1>
-
-                </div>
-
-                <div class="taglist">
-
-                    <a href="poll.php"><div class="pollList"><h2>Qui sera le dauphin de cette année ?</h2></div></a>
-
-                    <a href="poll.php"><div class="pollList"><h2>Qui sera le champion constructeur cette année ?</h2></div></a>
-
-                    <a href="poll.php"><div class="pollList"><h2>Qui deviendra champion du monde cette année ?</h2></div></a>
+                <section class="category">
 
 
-                </div>
+                    <div class="tag">
 
-            </section>
+                        <h1>#KohLanta</h1>
+
+                    </div>
+
+                    <div class="taglist">
+
+                        <a href="poll.php"><div class="pollList"><h2>Quelle équipe va gagner ce soir ?</h2></div></a>
+
+                        <a href="poll.php"><div class="pollList"><h2>Qui va se faire éliminer ?</h2></div></a>
+
+
+                    </div>
+
+                </section>
+
+                <section class="category">
+
+
+                    <div class="tag">
+
+                        <h1>#Formule1</h1>
+
+                    </div>
+
+                    <div class="taglist">
+
+                        <a href="poll.php"><div class="pollList"><h2>Qui sera le dauphin de cette année ?</h2></div></a>
+
+                        <a href="poll.php"><div class="pollList"><h2>Qui sera le champion constructeur cette année ?</h2></div></a>
+
+                        <a href="poll.php"><div class="pollList"><h2>Qui deviendra champion du monde cette année ?</h2></div></a>
+
+
+                    </div>
+
+                </section>
 
 
 
 
-        </main>
+            </main>
 
-       
+        
     </body>
 </html>
