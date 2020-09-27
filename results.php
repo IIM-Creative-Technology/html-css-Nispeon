fd2d6e93b0e5bb8c0f1d1c674918315508fb9fbe
Julien <Nisouc@outlook.com>
2020-09-27 19:57:59 +0200

    scoreboard arranged

--- results.php
+++ results.php
@@ -22,6 +22,43 @@
 
                 <div class="scoreboard">
 
+                    <div><h2>Classement général</h2></div>
+
+                    <div>
+                        <div class="mini"><p>1.</p></div> 
+                        <div class="mini"><h3>Nisouc</h3></div> 
+                        <div class="mini"><p>100 pts</p></div> 
+                    </div>
+
+                    <div>
+                        <div class="mini"><p>2.</p></div>  
+                        <div class="mini"><h3>A$ura</h3></div>  
+                        <div class="mini"><p>95 pts</p></div> 
+                    </div>
+
+                    <div>
+                        <div class="mini"><p>3.</p></div>  
+                        <div class="mini"><h3>Lollydroop</h3></div>  
+                        <div class="mini"><p>80 pts</p></div> 
+                    </div>
+
+                    <div>
+                        <div class="mini"><p>4.</p></div>  
+                        <div class="mini"><h3>Zekray</h3></div>  
+                        <div class="mini"><p>75 pts</p></div> 
+                    </div>
+
+                    <div>
+                        <div class="mini"><p>5.</p></div>  
+                        <div class="mini"><h3>Creepail</h3></div>  
+                        <div class="mini"><p>60 pts</p></div> 
+                    </div>
+
+                    <div>
+                        <div class="mini"><p>6.</p></div>  
+                        <div class="mini"><h3>Skupy</h3></div>  
+                        <div class="mini"><p>40 pts</p></div> 
+                    </div>
                 </div>
 
                 <div class="scorePers">
