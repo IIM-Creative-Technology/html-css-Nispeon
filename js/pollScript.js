042b36a017a55bb18c3824ead01302d57b10ea8e
Julien <Nisouc@outlook.com>
2020-10-02 17:49:43 +0200

    rework for js

---
+++ js/pollScript.js
@@ -0,0 +1,8 @@
+var sondage1 = [ 
+    
+    questionTitle = 'Quelle équipe va gagner ce soir ?', 
+    answers = [ 
+        {choice : 'Paris Eternal', isRight : true},
+        {choice : 'Houston Outlaws', isRight : false}
+    ],
+];
